e760888410baaefdc5bd5243e928e1ebe78f5a70
rmd09 <rmd09@yandex.ru>
2024-04-27 12:37:05 +0300

    config.js

--- database/connect.js
+++ database/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const DB_URL = "mongodb://localhost:27017/pindie";
+const { DB_URL } = require("../api/config");
 
 async function connectToDatabase() {
     try {
