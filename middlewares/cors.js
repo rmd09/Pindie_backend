2fbc6f43d8cba043a952151bf84889912123c492
rmd09 <rmd09@yandex.ru>
2024-05-23 18:27:08 +0300

    Delete unnecessary cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,4 @@
 const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'http://localhost:3000',
-    'http://localhost:3001',
     'https://frontpindie.nomoredomainswork.ru'
 ];
 
